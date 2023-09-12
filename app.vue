8af708b2279c2569881dde2af98b624a7e464818
jd-solanki <jdsolanki0001@gmail.com>
2023-09-12 19:43:33 +0530

    init

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+/*
+  1. We don't get auto completion for API endpoint with custom fetch
+  2. Type error when defining default value for data
+*/
+const { data } = await useCustomFetch<Record<string, any>>('/hello', {
+  default: () => {},
+})
+</script>
+
+<template>
+  <div>
+    {{ data }}
+  </div>
+</template>
